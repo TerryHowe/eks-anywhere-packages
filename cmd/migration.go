0e72797a8eb5248dcce953176f725d99de731e0d
Terry Howe <tlhowe@amazon.com>
2022-11-29 14:04:17 -0700

    Add --cluster-name option

--- cmd/migration.go
+++ cmd/migration.go
@@ -15,27 +15,31 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
 	"k8s.io/apimachinery/pkg/runtime"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	"os"
-	"path/filepath"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/aws/eks-anywhere-packages/api/v1alpha1"
-	"github.com/spf13/cobra"
 )
 
 type migrateContext struct {
+	clusterName string
 }
 
 var migrateCommandContext = &migrateContext{}
 
 func init() {
 	rootCmd.AddCommand(migrateCommand)
+	clusterName := os.Getenv("CLUSTER_NAME")
+	migrateCommand.Flags().StringVar(&migrateCommandContext.clusterName, "cluster-name", clusterName,
+		"Name of the management cluster.")
 }
 
 func getKubeConfig() string {
@@ -47,13 +51,11 @@ func getKubeConfig() string {
 }
 
 func migrate() error {
-	ctx := context.Background()
-
-	clusterName := os.Getenv("CLUSTER_NAME")
-	if len(clusterName) == 0 {
-		return fmt.Errorf("CLUSTER_NAME environment variable is not set or exported")
+	ctx := rootCmd.Context()
+	if len(migrateCommandContext.clusterName) == 0 {
+		return fmt.Errorf("management cluster name must be specified with --cluster-name option")
 	}
-	clusterNamespace := "eksa-packages-" + clusterName
+	clusterNamespace := "eksa-packages-" + migrateCommandContext.clusterName
 
 	config, err := rest.InClusterConfig()
 	if err != nil {
